f9a4a41e8f0ca6799c4aea0b2d8157216272b925
chryschi <121822197+chryschi@users.noreply.github.com>
2023-03-30 23:48:28 +0200

    Create new grid with user prompt

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
 const container = document.querySelector("#container");
 
 // const div = document.createElement("div");
+function createGrid(size = 4) {
+  for (let i = 0; i < size * size; ++i) {
+    const div = document.createElement("div");
+    div.setAttribute("id", `${i + 1}`);
 
-for (let i = 0; i < 16; ++i) {
-  const div = document.createElement("div");
-  div.setAttribute("id", `${i + 1}`);
-
-  container.appendChild(div);
+    container.appendChild(div);
+  }
+  const divs = document.querySelectorAll("#container > div");
+  divs.forEach((div) =>
+    div.addEventListener("mouseover", (e) => {
+      e.target.style.cssText = "background-color: black;";
+    })
+  );
 }
 
-const divs = document.querySelectorAll("#container > div");
-divs.forEach((div) =>
-  div.addEventListener("mouseover", (e) => {
-    e.target.style.cssText = "background-color: black;";
-  })
-);
+createGrid();
+
+function changeGrid() {
+  let newGridSize = window.prompt();
+  while (newGridSize >= 100) {
+    newGridSize = window.prompt("Grid Size must be smaller than 100");
+  }
+  console.log(newGridSize);
+  console.log(container.hasChildNodes());
+  while (container.hasChildNodes()) {
+    container.removeChild(container.firstChild);
+  }
+  console.log(container.hasChildNodes());
+  console.log(newGridSize);
+  createGrid(newGridSize);
+}
+
+const button = document.querySelector("button");
+button.addEventListener("click", () => changeGrid());
